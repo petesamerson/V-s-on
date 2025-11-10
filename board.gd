50de63451f6335bb9abbdc79c9f7a2efd5be4038
petesamerson <swpeterson623@comcast.net>
2025-11-10 14:42:08 -0500

    create board animation

--- board.gd
+++ board.gd
@@ -6,10 +6,60 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var timer := 0.0
+var interval := 0.25 #seconds
+
 func _process(delta: float) -> void:
-	pass
+	timer += delta
+	if timer >= interval:
+		timer = 0.0
+		animate_board()
 
 var player = 1
+
+var cur_ani_x = 0
+var cur_ani_y = 0
+var x_ani = true
+var ani_index = 0
+var ani_offset = 0
+func animate_board():
+	var cur_range = (cur_ani_x + 1)*2
+	if(cur_range == 0):
+		cur_range = 2
+	for i in range(cur_range):
+		set_cell(Vector2i(cur_ani_x - ani_offset,cur_ani_y), 12, Vector2i(0,0))
+		print(["curAni xy", cur_ani_x, cur_ani_y, "index|offset", ani_index, ani_offset])
+		if(cur_ani_y % 2 == 1):
+			ani_offset += 1
+		cur_ani_y += 1
+	cur_ani_y = 0
+	ani_offset = 0 
+	cur_ani_x += 1
+		# set_cell(Vector2i(center.x - offset_neg, center.y + i), line_color_id, Vector2i(0,0))
+	# set_cell(Vector2i(center.x + offset_pos, center.y - i), line_color_id, Vector2i(0,0))
+	# if (cur_ani_y) % 2 == 0:
+	# 	ani_offset += 2
+	# if(cur_ani_y >= cur_ani_x*2):
+	# 	cur_ani_y = 0
+	# 	cur_ani_x += 1
+	# 	ani_offset = 1
+
+	# if(x_ani):
+	# 	cur_ani_x += 1
+	# else:
+	# 	cur_ani_y +=1
+
+	# if(cur_ani_x > 15):
+	# 	x_ani = false
+	# 	ani_index += 1
+	# 	cur_ani_x  = ani_index
+
+	# if(cur_ani_y > 15):
+	# 	x_ani = true
+	# 	# ani_index += 1
+	# 	cur_ani_y  = ani_index
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -18,12 +68,13 @@ func _unhandled_input(event: InputEvent) -> void:
 
 		set_cell(
 			cell, 
-			12,
+			5,
 			Vector2i(0,0)
 		)
 		draw_hex_around(cell)
-		# draw_horz_line(cell)
+		draw_horz_line(cell)
 		draw_forward_dia_line(cell)
+		draw_back_dia_line(cell)
 		
 		if(player == 1) :
 			player = 2
@@ -115,7 +166,7 @@ func draw_horz_line(center: Vector2i):
 		set_cell(Vector2i(center.x - i, center.y), line_color_id, Vector2i(0,0))
 
 func draw_back_dia_line(center: Vector2i):
-	var line_color_id = 5
+	var line_color_id = 8
 	var offset_pos = 0
 	if (center.y) % 2 == 0:
 		offset_pos = 1
@@ -134,7 +185,7 @@ func draw_back_dia_line(center: Vector2i):
 			offset_neg += 1
 		
 func draw_forward_dia_line(center: Vector2i):
-	var line_color_id = 4
+	var line_color_id = 12
 	var offset_pos = 0
 	if (center.y) % 2 == 0:
 		offset_pos = 1
